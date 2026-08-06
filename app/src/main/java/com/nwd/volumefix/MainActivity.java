b6b554cbe9285e54970e4efd07ef7fb742ce40bd
EmreBoz13 <103121961+EmreBoz13@users.noreply.github.com>
2026-08-06 03:28:47 +0300

    Add files via upload

---
+++ app/src/main/java/com/nwd/volumefix/MainActivity.java
@@ -0,0 +1,29 @@
+package com.nwd.volumefix;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+
+        Button button = new Button(this);
+        button.setText("SES BARINI AÇ");
+
+        button.setOnClickListener(v -> {
+            try {
+                Intent i = new Intent("com.nwd.volumeview.VolumeService");
+                startService(i);
+                Toast.makeText(this, "Ses barı komutu gönderildi", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        setContentView(button);
+    }
+}
